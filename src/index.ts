302b956eaabd2a1db15ef2aa6367d1b7c1b62bb8
Tyler Long <tyler4long@gmail.com>
2020-06-08 00:48:13 -0700

    Display image

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Matter from 'matter-js';
 
+import boxImg from './box.png';
+
 const {Engine, Bodies, World, Render} = Matter;
 
 // engine
@@ -7,8 +9,24 @@ const engine = Engine.create();
 Engine.run(engine);
 
 // bodies and world
-const boxA = Bodies.rectangle(400, 200, 80, 80);
-const boxB = Bodies.rectangle(450, 50, 80, 80);
+const boxA = Bodies.rectangle(400, 200, 80, 80, {
+  render: {
+    sprite: {
+      texture: boxImg,
+      xScale: 1.2,
+      yScale: 1.2,
+    },
+  },
+});
+const boxB = Bodies.rectangle(400, 50, 80, 80, {
+  render: {
+    sprite: {
+      texture: boxImg,
+      xScale: 1.2,
+      yScale: 1.2,
+    },
+  },
+});
 const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
 World.add(engine.world, [boxA, boxB, ground]);
 
@@ -16,5 +34,8 @@ World.add(engine.world, [boxA, boxB, ground]);
 const render = Render.create({
   element: document.body,
   engine: engine,
+  options: {
+    wireframes: false,
+  },
 });
 Render.run(render);
